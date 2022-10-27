132be8a9591d14f8d3e2d78fae925c5c8fc0f085
BULAT <ibragimovbi@yandex.ru>
2022-10-27 18:25:05 +0300

    Задания 1 и 2

---
+++ src/Person.java
@@ -0,0 +1,22 @@
+
+
+
+
+public class Person {
+
+    int yearOfBirth;
+    String name;
+    String town;
+    String jobTitle;
+
+    public Person(String name, int yearOfBirth, String town, String jobTitle) {
+        this.yearOfBirth = yearOfBirth;
+        this.name = name;
+        this.town = town;
+        this.jobTitle = jobTitle;
+    }
+
+    public void acquaintance () {
+        System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности " + jobTitle + ". Будем знакомы!");
+    }
+}
